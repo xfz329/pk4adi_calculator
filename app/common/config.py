7c705b89c51b68973e4d12cb7155dbbfc66007d2
xfz329 <495390131@qq.com>
2023-07-15 09:10:44 +0800

    setting interface

---
+++ app/common/config.py
@@ -0,0 +1,58 @@
+# coding:utf-8
+from enum import Enum
+
+from PyQt5.QtCore import QLocale
+from qfluentwidgets import (qconfig, QConfig, ConfigItem, OptionsConfigItem, BoolValidator,
+                            OptionsValidator, RangeConfigItem, RangeValidator,
+                            FolderListValidator, EnumSerializer, FolderValidator, ConfigSerializer, __version__)
+
+
+class Language(Enum):
+    """ Language enumeration """
+
+    CHINESE_SIMPLIFIED = QLocale(QLocale.Chinese, QLocale.China)
+    CHINESE_TRADITIONAL = QLocale(QLocale.Chinese, QLocale.HongKong)
+    ENGLISH = QLocale(QLocale.English)
+    AUTO = QLocale()
+
+
+class LanguageSerializer(ConfigSerializer):
+    """ Language serializer """
+
+    def serialize(self, language):
+        return language.value.name() if language != Language.AUTO else "Auto"
+
+    def deserialize(self, value: str):
+        return Language(QLocale(value)) if value != "Auto" else Language.AUTO
+
+
+class Config(QConfig):
+    """ Config of application """
+
+    # folders
+    outputFolder = ConfigItem(
+        "Folders", "Output", "output", FolderValidator())
+
+    # main window
+    dpiScale = OptionsConfigItem(
+        "MainWindow", "DpiScale", "Auto", OptionsValidator([1, 1.25, 1.5, 1.75, 2, "Auto"]), restart=True)
+    language = OptionsConfigItem(
+        "MainWindow", "Language", Language.AUTO, OptionsValidator(Language), LanguageSerializer(), restart=True)
+
+    # software update
+    checkUpdateAtStartUp = ConfigItem("Update", "CheckUpdateAtStartUp", True, BoolValidator())
+
+
+YEAR = 2023
+AUTHOR = "Jiang Feng"
+VERSION = __version__
+HELP_URL = "https://pyqt-fluent-widgets.readthedocs.io/zh_CN/latest"
+REPO_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets"
+EXAMPLE_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets/tree/master/examples"
+FEEDBACK_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets/issues"
+RELEASE_URL = "https://github.com/zhiyiYo/PyQt-Fluent-Widgets/releases/latest"
+SUPPORT_URL = "https://afdian.net/a/zhiyiYo"
+
+
+cfg = Config()
+qconfig.load('config/config.json', cfg)
