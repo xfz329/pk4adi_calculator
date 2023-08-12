addb64ac81f3e84ed1dc8ca3e601514a10df77a3
xfz329 <495390131@qq.com>
2023-08-13 04:10:17 +0800

    0.1.4.c

--- nsis/output.cmd
+++
@@ -1,5 +0,0 @@
-pyinstaller.exe  -i D:\UrgeData\Documents\Codes\Github\pk_gui\figures\pk.ico -Dw main.py -n PK4ADI
-pyinstaller.exe  -i D:\UrgeData\Documents\Codes\Github\pk_gui\resource\images\logo.ico -Dw pk4adi_demo.py -n PK4ADI
-cp -r D:\UrgeData\Documents\Codes\Github\pk_gui\resource\ D:\UrgeData\Documents\Codes\Github\pk_gui\dist\PK4ADI\resource
-cp -r D:\UrgeData\Documents\Codes\Github\pk_gui\nsis\logs D:\UrgeData\Documents\Codes\Github\pk_gui\dist\PK4ADI
-cp -r D:\UrgeData\Documents\Codes\Github\pk_gui\license D:\UrgeData\Documents\Codes\Github\pk_gui\dist\PK4ADI
